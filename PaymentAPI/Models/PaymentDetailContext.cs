de1743be294cd829ce1e4fb15f9573d8703525b7
marcelotakayama <marcelo.takayama00@gmail.com>
2021-02-05 15:40:20 -0300

    first

---
+++ PaymentAPI/Models/PaymentDetailContext.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PaymentAPI.Models {
+    public class PaymentDetailContext : DbContext {
+    }
+}
