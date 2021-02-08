c709cf30b7d12bb8bd78ffd2fdca95a6f9c8a4b7
marcelotakayama <marcelo.takayama00@gmail.com>
2021-02-08 16:53:51 -0300

    organizando o codigo

--- PaymentAPI/Models/PaymentDetail.cs
+++ PaymentAPI/Models/PaymentDetail.cs
@@ -5,12 +5,16 @@ namespace PaymentAPI.Models {
     public class PaymentDetail {
         [Key]
         public int PaymentDetailId { get; set; }
+
         [Column (TypeName ="nvarchar(100)")]
         public string CardOwnerName { get; set; }
+
         [Column (TypeName ="nvarchar(16)")]
         public string CardNumber { get; set; }
+
         [Column (TypeName ="nvarchar(5)")]
         public string ExpirationDate { get; set; }
+
         [Column (TypeName ="nvarchar(3)")]
         public string SecurityCode { get; set; }
     }
